6ff8aade7ed9582033915bd3b0bb2e6aad0a021c
Alexis <b04705010@ntu.edu.tw>
2018-08-21 10:14:42 +0800

    timer

--- app.py
+++ app.py
@@ -1,133 +1,32 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 from pprint import pprint
+from rocketchat_API.rocketchat import RocketChat
 import logging
 import requests
+import random
+import datetime
 
 app = Flask(__name__)
 api = Api(app)
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+def ranSrc():
+    src = [{"MonitorPhase":"Createitem","IsDelay":"True","DelayCount":"1"},{"MonitorPhase":"Inventory","IsDelay":"False","DelayCount":"0"}]
+    x0 = random.randint(0,1)
+    x1 = random.randint(0,1)
+    y0 = "True" if x0 else "False"
+    y1 = "True" if x1 else "False"
+    src[0]["IsDelay"] = y0
+    src[1]["IsDelay"] = y1
+    src[0]["DelayCount"] = str(x0)
+    src[1]["DelayCount"] = str(x1)
+    return src
 
-class RocketException(Exception):
-    pass
-
-
-class RocketConnectionException(Exception):
-    pass
-
-
-class RocketAuthenticationException(Exception):
-    pass
-
-
-class RocketMissingParamException(Exception):
-    pass
-
-
-
-class RocketChat:
-    headers = {}
-    API_path = '/api/v1/'
-
-    def __init__(self, user=None, password=None, server_url='http://127.0.0.1:3000', ssl_verify=True, proxies=None,
-                 timeout=30):
-        """Creates a RocketChat object and does login on the specified server"""
-        self.server_url = server_url
-        self.proxies = proxies
-        self.ssl_verify = ssl_verify
-        self.timeout = timeout
-        if user and password:
-            self.login(user, password)
-
-    @staticmethod
-    def __reduce_kwargs(kwargs):
-        if 'kwargs' in kwargs:
-            for arg in kwargs['kwargs'].keys():
-                kwargs[arg] = kwargs['kwargs'][arg]
-
-            del kwargs['kwargs']
-        return kwargs
-
-    def __call_api_get(self, method, **kwargs):
-        args = self.__reduce_kwargs(kwargs)
-        return requests.get(self.server_url + self.API_path + method + '?' +
-                            '&'.join([i + '=' + str(args[i])
-                                      for i in args.keys()]),
-                            headers=self.headers,
-                            verify=self.ssl_verify,
-                            proxies=self.proxies,
-                            timeout=self.timeout
-                            )
-
-    def __call_api_post(self, method, files=None, use_json=True, **kwargs):
-        reduced_args = self.__reduce_kwargs(kwargs)
-        # Since pass is a reserved word in Python it has to be injected on the request dict
-        # Some methods use pass (users.register) and others password (users.create)
-        if 'password' in reduced_args and method != 'users.create':
-            reduced_args['pass'] = reduced_args['password']
-        if use_json:
-            return requests.post(self.server_url + self.API_path + method,
-                                 json=reduced_args,
-                                 files=files,
-                                 headers=self.headers,
-                                 verify=self.ssl_verify,
-                                 proxies=self.proxies,
-                                 timeout=self.timeout
-                                 )
-        else:
-            return requests.post(self.server_url + self.API_path + method,
-                                 data=reduced_args,
-                                 files=files,
-                                 headers=self.headers,
-                                 verify=self.ssl_verify,
-                                 proxies=self.proxies,
-                                 timeout=self.timeout
-                                 )
-
-    def login(self, user, password):
-        login_request = requests.post(self.server_url + self.API_path + 'login',
-                                      data={'username': user,
-                                            'password': password},
-                                      verify=self.ssl_verify,
-                                      proxies=self.proxies)
-        if login_request.status_code == 401:
-            raise RocketAuthenticationException()
-
-        if login_request.status_code == 200:
-            if login_request.json().get('status') == "success":
-                self.headers['X-Auth-Token'] = login_request.json().get('data').get('authToken')
-                self.headers['X-User-Id'] = login_request.json().get('data').get('userId')
-                return login_request
-            else:
-                raise RocketAuthenticationException()
-        else:
-            raise RocketConnectionException()
-
-    def me(self, **kwargs):
-        """	Displays information about the authenticated user."""
-        return self.__call_api_get('me', kwargs=kwargs)
-
-    def chat_post_message(self, text, room_id=None, channel=None, attachments=None, **kwargs):
-        """Posts a new chat message."""
-        if room_id:
-            return self.__call_api_post('chat.postMessage', roomId=room_id, text=text, attachments=attachments, kwargs=kwargs)
-        elif channel:
-            return self.__call_api_post('chat.postMessage', channel=channel, text=text, attachments=attachments, kwargs=kwargs)
-        else:
-            raise RocketMissingParamException('roomId or channel required')
-    def channels_list(self, **kwargs):
-        """Retrieves all of the channels from the server."""
-        return self.__call_api_get('channels.list', kwargs=kwargs)
-
-
-
-def sendMessage(mes,attachments,cha="integration_test"):
+def sendMessage(mes,cha="integration_test"):
     rocket = RocketChat('aw5p', '@lexis1437', server_url='https://chat.newegg.org')
     pprint(rocket.me().json())
     pprint(rocket.channels_list().json())
-    pprint(rocket.chat_post_message(mes, channel=cha, alias='AlertBot', attachments=attachments).json())
+    pprint(rocket.chat_post_message(mes, channel=cha, alias='AlertBot').json())
 
 class AlertAPI(Resource):
     def post(self):
@@ -136,16 +35,48 @@ class AlertAPI(Resource):
         if (int(src[0]["DelayCount"])+int(src[1]["DelayCount"])):
             title = "CI: "+src[0]["DelayCount"]+" ,I: "+src[1]["DelayCount"]
             text = src[0]["MonitorPhase"]+"__IsDelay: "+src[0]["IsDelay"]+", DelayCount = "+src[0]["DelayCount"]+"\n"+src[1]["MonitorPhase"]+"__IsDelay: "+src[1]["IsDelay"]+", DelayCount = "+src[1]["DelayCount"]
-            att = {}
-            att["title"] = title
-            att["text"] = text
-            att["color"] = "#FF8800"
-            att = [att]
-            sendMessage("Alert", att)
+            sendMessage(text)
             return "success"
         return "fail"
 
-api.add_resource(AlertAPI, '/api/alert')
+parser = reqparse.RequestParser()
+parser.add_argument('n', type=int)
+
+class Test(Resource):
+    lastRecord = {}
+
+    def post(self):
+        src = ranSrc()
+        totalDelay = 0
+        text = ""
+        
+        """
+        now = datetime.datetime.now()
+        if now.minute == 0:
+            text += "Top of the hour Alert\n"
+            for phase in src:
+                text += phase["MonitorPhase"]+"-Delay Count = "+phase["DelayCount"]+"\n"
+                self.lastRecord[phase["MonitorPhase"]] = phase["DelayCount"]
+                totalDelay += int(phase["DelayCount"])
+            sendMessage(text)
+            return
+        """
+
+        for phase in src:
+            try:
+                if self.lastRecord[phase["MonitorPhase"]] !="0" or phase["IsDelay"] == "True":
+                    text += phase["MonitorPhase"]+"-Delay Count = "+phase["DelayCount"]+"\n"
+                self.lastRecord[phase["MonitorPhase"]] = phase["DelayCount"]
+                totalDelay += int(phase["DelayCount"])
+            except:
+                if phase["IsDelay"] == "True":
+                    text += phase["MonitorPhase"]+"-Delay Count = "+phase["DelayCount"]+"\n"
+                self.lastRecord[phase["MonitorPhase"]] = phase["DelayCount"]
+                totalDelay += int(phase["DelayCount"])
+        return if totalDelay == 0 else sendMessage(text)
+        return self.lastRecord
+
+api.add_resource(Test, '/api/Alert')
 
 
 if __name__ == '__main__':
